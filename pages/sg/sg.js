167c4f023edf46a15b4b6ff26631b4dddc323904
zjc <2353409885@qq.com>
2018-09-09 12:31:20 +0800

    新增:操作结果提示/loading

--- pages/sg/sg.js
+++ pages/sg/sg.js
@@ -9,7 +9,10 @@ Page({
    */
   data: {
     oldValue :"", 
-    value:'0' 
+    value:'0' ,
+    DataValue:{
+      value:'zjc'
+    }
   },
 
   /**
@@ -20,13 +23,19 @@ Page({
         this.setData({
           oldValue:"这个是原始值"
         })
+      
+     
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    //演示-页面加载时使用的方法
+    wx.showLoading({
+      title: '欢迎进入sg页面',
+    })
+   
   },
 
   /**
@@ -70,6 +79,7 @@ Page({
   onShareAppMessage: function () {
 
   },
+  //演示-数据的获取与修改
   changeValue:function(e){
     //实现新值与原始值之间的切换
     if (0 == e.currentTarget.dataset.value){
@@ -77,14 +87,34 @@ Page({
         oldValue: "这个是新值",
         value: 1
       })
+      //演示-操作成功之后-信息提示
+      wx.showToast({
+        title: '显示新值成功!',
+      })
     }else{
       this.setData({
         oldValue: "这个是原始值",
         value: 0
       })
+      //演示-操作成功之后-信息提示
+      wx.showToast({
+        title: '显示原始值成功!',
+      })
     }
     
    
+  },
+  //演示获取page中data数据
+  getPageData:function(){
+    //获取方式
+    //代码
+    //this.data.DataValue.value
+    
+  },
+  //演示-手动关闭loading
+  closeLoading:function(){
+    //页面加载结束.loading隐藏
+    wx.hideLoading();
   }
 
 })
